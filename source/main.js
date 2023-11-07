4d5805f4aee625c2282a37a5e014641ebe6d929c
Rob Anderson <robanderson@hey.com>
2023-11-07 14:02:13 +0000

    organise json + svg + add popups

--- source/main.js
+++ source/main.js
@@ -3,6 +3,25 @@ import floor from "./floor.json" assert { type: "json" };
 import { ColorInterpolator } from "./color-interpolator.js";
 import { PointImageInterpolator } from "./point-image-interpolator.js";
 
+/**
+  @typedef {Object} Point
+  @property {number} x
+  @property {number} y
+  @property {number | null | undefined} value
+  @property {HTMLElement} element
+*/
+
+/**
+  @typedef {Object} ZoneRecord
+  @property {PointImageInterpolator} interpolator
+  @property {HTMLElement} elem
+  @property {Point[]} points
+  @property {(typeof floor)['spaces'][number]} space
+  @property {Function} draw
+  @property {Point} point
+  @property {(typeof floor)['spaces'][number]['zones'][number]} zone
+*/
+
 /** @type {SVGScriptElement} */
 let svg = null;
 
@@ -13,14 +32,22 @@ const UO_BASE =
 
 const canvas = document.getElementById("canvas");
 
+/** @type {HTMLDialogElement} */
+const popup = document.getElementById("popup");
+
+let isFetching = false;
+
 const noValueColour = "transparent";
 const mode = "room-temperature";
+
+/** @type {Map<string, HTMLElement>} */
 const rooms = new Map();
-const spaces = new Map();
+
+/** @type {Map<string, ZoneRecord>} */
 const zones = new Map();
 
 // Color stops for expected temperature range
-const temperatureColours = new ColorInterpolator([
+const temperatureColors = new ColorInterpolator([
   { offset: 0, color: "#663399" },
   { offset: 16, color: "#82c3d0" },
   { offset: 18, color: "#ABCE82" },
@@ -29,30 +56,7 @@ const temperatureColours = new ColorInterpolator([
   { offset: 24, color: "#fd3814" },
   { offset: 42.5, color: "#770600" },
 ]);
-// const temperatureColours = new ColorInterpolator([
-//   { offset: -26, color: "#000000" }, // black
-//   { offset: 0, color: "#663399" }, // dark purple
-//   { offset: 16, color: "#333399" }, // dark blue
-//   { offset: 18, color: "#339999" }, // turquoise
-//   { offset: 20, color: "#339966" }, // sea green
-//   { offset: 22, color: "#dddd00" }, // yellow
-//   { offset: 24, color: "#dd6600" }, // red-orange
-//   { offset: 26, color: "#dd3300" }, // dark red
-//   { offset: 32, color: "#ff0000" }, // bright red
-//   { offset: 42, color: "#ff00ff" }, // magenta
-// ]);
-const temperatureLookup = temperatureColours.createLookup(1024);
-
-function interpolate(minValue, maxValue, minOut, maxOut, fn) {
-  return (value) => {
-    // Calculate the hue value based on the temperature
-    // prettier-ignore
-    return fn(((value - minValue) / (maxValue - minValue)) * (maxOut - minOut) + minOut);
-
-    // Convert the hue to an RGB color
-    // return `hsl(${hue}, 100%, 70%)`;
-  };
-}
+const temperatureLookup = temperatureColors.createLookup(1024);
 
 function debounce(ms, fn) {
   let timerId = null;
@@ -66,20 +70,11 @@ function debounce(ms, fn) {
 }
 
 const gradients = {
-  // "room-temperature": interpolate(
-  //   15,
-  //   25,
-  //   200,
-  //   100,
-  //   (hue) => `hsl(${hue}, 100%, 70%)`,
-  // ),
   "room-temperature"(value) {
     return ColorInterpolator.toColorString(
-      temperatureColours.interpolateColor(value),
+      temperatureColors.interpolateColor(value),
     );
   },
-  co2: interpolate(300, 500, 120, 0, (hue) => `hsl(${hue}, 100%, 70%)`),
-  "room-brightness": interpolate(0, 100, 0, 1, (v) => `rgb(${v}, ${v}, ${v})`),
 };
 
 async function main() {
@@ -90,9 +85,9 @@ async function main() {
   canvas.appendChild(svg);
 
   for (const room of floor.rooms) {
-    const elem = svg.querySelector(room.selector);
+    const elem = svg.querySelector(room.floorSelector);
     if (!elem) {
-      console.error("room not found", room.selector);
+      console.error("room not found", room.floorSelector);
       continue;
     }
     rooms.set(room.entityId, elem);
@@ -101,10 +96,10 @@ async function main() {
 
   // spaces
   for (const space of floor.spaces) {
-    const elem = svg.querySelector(space.selector);
+    const elem = svg.querySelector(space.floorSelector);
 
     if (!elem) {
-      console.error("room not found", space.selector);
+      console.error("room not found", space.floorSelector);
       continue;
     }
 
@@ -112,7 +107,6 @@ async function main() {
       elem,
       "circle",
     );
-    console.log(points);
     const interpolator = new PointImageInterpolator(elem);
 
     const record = {
@@ -120,11 +114,8 @@ async function main() {
       elem,
       points,
       space,
-      draw: debounce(500, () => {
-        interpolator.draw();
-      }),
+      draw: debounce(500, () => interpolator.draw()),
     };
-    spaces.set(space.selector, record);
 
     for (const zone of space.zones) {
       for (const point of points) {
@@ -142,7 +133,6 @@ async function main() {
   legend.setAttribute("role", "list");
 
   for (let i = 15; i <= 25; i++) {
-    console.log(i);
     const li = legend.appendChild(document.createElement("li"));
     const colour = li.appendChild(document.createElement("span"));
     li.appendChild(document.createTextNode(`${i}`));
@@ -150,7 +140,7 @@ async function main() {
     colour.classList.add("legendColour");
     colour.style.setProperty(
       "--colour",
-      ColorInterpolator.toColorString(temperatureColours.interpolateColor(i)),
+      ColorInterpolator.toColorString(temperatureColors.interpolateColor(i)),
     );
   }
 
@@ -165,38 +155,59 @@ async function main() {
   setInterval(() => fetchData(), 60_000);
 }
 
-async function fetchData() {
-  for (const room of floor.rooms) {
-    const res = await fetch(
-      new URL(`./sensors/entity/${room.entityId}`, UO_BASE),
-    );
-    if (!res.ok) continue;
+async function fetchEntity(entityId) {
+  const res = await fetch(
+    new URL(`./sensors/entity/${entityId}`, UO_BASE),
+  ).catch(() => null);
 
-    res.json().then((d) => onEntityData(d));
+  if (!res?.ok) {
+    console.error("fetch entity failed: " + res.statusText);
+    return null;
+  }
+
+  return res.json();
+}
+
+async function fetchFeed(feedId) {
+  const res = await fetch(new URL(`./sensors/feed/${feedId}`, UO_BASE)).catch(
+    () => null,
+  );
+
+  if (!res?.ok) {
+    console.error("fetch feed failed: " + res.statusText);
+    return null;
+  }
+
+  return res.json();
+}
+
+async function fetchData() {
+  if (isFetching) return;
+  isFetching = true;
+
+  for (const room of floor.rooms) {
+    if (!room.feeds[mode]) continue;
+    const data = await fetchFeed(room.feeds[mode]);
+    if (data) onFeedData(room.entityId, data);
   }
 
   for (const space of floor.spaces) {
     for (const zone of space.zones) {
-      const res = await fetch(
-        new URL(`./sensors/entity/${zone.entityId}`, UO_BASE),
-      );
-      if (!res.ok) continue;
-
-      res.json().then((d) => onEntityData(d));
+      if (!zone.feeds[mode]) continue;
+      const data = await fetchFeed(zone.feeds[mode]);
+      if (data) onFeedData(zone.entityId, data);
     }
   }
+  isFetching = false;
 }
 
-async function onEntityData(data) {
-  let elem = rooms.get(data.entityId);
-  const room = floor.rooms.find((r) => r.entityId === data.entityId);
-  const zone = zones.get(data.entityId);
+async function onFeedData(entityId, data) {
+  let elem = rooms.get(entityId);
+  const room = floor.rooms.find((r) => r.entityId === entityId);
+  const zone = zones.get(entityId);
+  const latest = data.timeseries?.[0]?.latest?.value;
 
   if (elem && room) {
-    const feedId = room.feeds[mode];
-    const feed = data.feed.find((f) => f.feedId === feedId);
-
-    const latest = feed?.timeseries?.[0]?.latest?.value;
     console.log(elem.id, latest);
 
     if (latest) {
@@ -208,17 +219,7 @@ async function onEntityData(data) {
     }
   }
 
-  // interpolator: new PointImageInterpolator(elem),
-  // elem,
-  // points,
-  // point
-  // draw
-
   if (zone) {
-    const feedId = zone.zone.feeds[mode];
-    const feed = data.feed.find((f) => f.feedId === feedId);
-
-    const latest = feed?.timeseries?.[0]?.latest?.value;
     console.debug(zone.zone.selector, latest);
 
     if (latest) {
@@ -231,39 +232,46 @@ async function onEntityData(data) {
   }
 }
 
-async function onRoomClick(_room, e) {
-  const title = e.target?.getAttribute("title");
-  if (title) alert(`Value: ${title}`);
+/** @param {(typeof floor)['rooms'][number]} room */
+async function onRoomClick(room) {
+  const entity = await fetchEntity(room.entityId);
+  if (!entity) return;
+
+  const label = document.querySelector(room.labelSelector);
+
+  const rows = [];
+  for (const feedId of Object.values(room.feeds)) {
+    const feed = entity.feed.find((f) => f.feedId === feedId);
+    const value = feed.timeseries[0]?.latest?.value;
+    const unit = feed.timeseries[0]?.unit?.name;
+
+    if (/occupancy/i.test(feed.metric)) {
+      rows.push(`<dt>${feed.metric}</dt>`);
+      rows.push(`<dd>${value ? "Yes" : "No"}</dd>`);
+    } else if (value) {
+      rows.push(`<dt>${feed.metric}</dt>`);
+      rows.push(`<dd>${value} ${unit ?? ""}</dd>`);
+    } else {
+      // rows.push(`<dd>no value</dd>`);
+    }
+  }
+
+  // popup.open = true;
+  popup.showModal();
+  popup.innerHTML = `
+    <h2>${label?.textContent ?? entity.name ?? "Room"}</h2>
+    <dl>${rows.join("\n")}</dl>
+    <cluster-layout class="toolbar">
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </cluster-layout>
+  `;
+
+  popup.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    popup.close();
+  });
 }
 
 main().catch((e) => console.error("fatal error", e));
-
-//
-//
-//
-
-// Element to draw interpolated image to
-// const element = document.querySelector("#shape");
-
-// Point interpolator
-// const pointImageInterpolator = new PointImageInterpolator(element);
-
-// Extract point coordinates from parent group matching 'circle'
-// const points = PointImageInterpolator.extractGroupSiblingPoints(
-//   element,
-//   "circle",
-// );
-//
-// // Draw and randomly animate points
-// async function update() {
-//   pointImageInterpolator.update(points, colorLookup);
-//   await pointImageInterpolator.draw();
-//   for (const point of points) {
-//     //point.x += Math.random() * 0.01 - 0.005;
-//     //point.y += Math.random() * 0.01 - 0.005;
-//     if (point.value == null) point.value = 20;
-//     point.value += Math.random() * 2 - 1;
-//   }
-// }
-// setInterval(() => update(), 100);
-// update();
