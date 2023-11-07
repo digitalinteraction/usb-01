68bcb955a020df746aefc7ace5334f977a73cf9e
Rob Anderson <robanderson@hey.com>
2023-11-07 14:50:03 +0000

    show other metrics

--- source/main.js
+++ source/main.js
@@ -35,10 +35,15 @@ const canvas = document.getElementById("canvas");
 /** @type {HTMLDialogElement} */
 const popup = document.getElementById("popup");
 
+/** @type {HTMLSelectField} */
+const metric = document.getElementById("metric");
+
 let isFetching = false;
 
 const noValueColour = "transparent";
-const mode = "room-temperature";
+
+/** @type {keyof typeof gradients} */
+let currentMetric = "room-temperature";
 
 /** @type {Map<string, HTMLElement>} */
 const rooms = new Map();
@@ -46,17 +51,39 @@ const rooms = new Map();
 /** @type {Map<string, ZoneRecord>} */
 const zones = new Map();
 
-// Color stops for expected temperature range
-const temperatureColors = new ColorInterpolator([
-  { offset: 0, color: "#663399" },
-  { offset: 16, color: "#82c3d0" },
-  { offset: 18, color: "#ABCE82" },
-  { offset: 20, color: "#FECB01" },
-  { offset: 22, color: "#FC7F0A" },
-  { offset: 24, color: "#fd3814" },
-  { offset: 42.5, color: "#770600" },
-]);
-const temperatureLookup = temperatureColors.createLookup(1024);
+const gradients = {
+  "room-temperature": createGradient(
+    [
+      { offset: 0, color: "#663399" },
+      { offset: 16, color: "#82c3d0" },
+      { offset: 18, color: "#ABCE82" },
+      { offset: 20, color: "#FECB01" },
+      { offset: 22, color: "#FC7F0A" },
+      { offset: 24, color: "#fd3814" },
+      { offset: 42.5, color: "#770600" },
+    ],
+    [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+  ),
+  "occupancy-sensor": createGradient(
+    [
+      { offset: 0, color: "#000000" },
+      { offset: 1, color: "#FFFFFF" },
+    ],
+    [0, 1],
+  ),
+
+  // https://www.kane.co.uk/knowledge-centre/what-are-safe-levels-of-co-and-co2-in-rooms
+  co2: createGradient(
+    [
+      { offset: 0, color: "#000000" },
+      { offset: 500, color: "#00FF00" },
+      { offset: 1_000, color: "#0000FF" },
+      { offset: 2_000, color: "#FF0000" },
+      { offset: 5_000, color: "#000000" },
+    ],
+    [0, 500, 1500, 2000, 5000],
+  ),
+};
 
 function debounce(ms, fn) {
   let timerId = null;
@@ -69,13 +96,11 @@ function debounce(ms, fn) {
   };
 }
 
-const gradients = {
-  "room-temperature"(value) {
-    return ColorInterpolator.toColorString(
-      temperatureColors.interpolateColor(value),
-    );
-  },
-};
+function createGradient(stops, legend, numSteps = 1024) {
+  const colors = new ColorInterpolator(stops);
+  const lookup = colors.createLookup(numSteps);
+  return { colors, lookup, legend };
+}
 
 async function main() {
   svg = new DOMParser()
@@ -127,22 +152,14 @@ async function main() {
     }
   }
 
-  const legend = document
-    .getElementById("legend")
-    .appendChild(document.createElement("ul"));
-  legend.setAttribute("role", "list");
+  drawLegend();
 
-  for (let i = 15; i <= 25; i++) {
-    const li = legend.appendChild(document.createElement("li"));
-    const colour = li.appendChild(document.createElement("span"));
-    li.appendChild(document.createTextNode(`${i}`));
-
-    colour.classList.add("legendColour");
-    colour.style.setProperty(
-      "--colour",
-      ColorInterpolator.toColorString(temperatureColors.interpolateColor(i)),
-    );
-  }
+  metric.addEventListener("input", async () => {
+    currentMetric = metric.value;
+    drawLegend();
+    await fetchData();
+    // TODO: should fetches be in a Q?
+  });
 
   svg.addEventListener("click", (e) => {
     const rect = e.target.getBoundingClientRect();
@@ -153,6 +170,26 @@ async function main() {
 
   await fetchData();
   setInterval(() => fetchData(), 60_000);
+}
+
+function drawLegend() {
+  const ul = document.getElementById("legend");
+  ul.innerHTML = "";
+  ul.setAttribute("role", "list");
+
+  const { colors, legend } = gradients[currentMetric];
+
+  for (const i of legend) {
+    const li = ul.appendChild(document.createElement("li"));
+    const colour = li.appendChild(document.createElement("span"));
+    li.appendChild(document.createTextNode(`${i}`));
+
+    colour.classList.add("legendColour");
+    colour.style.setProperty(
+      "--colour",
+      ColorInterpolator.toColorString(colors.interpolateColor(i)),
+    );
+  }
 }
 
 async function fetchEntity(entityId) {
@@ -186,15 +223,15 @@ async function fetchData() {
   isFetching = true;
 
   for (const room of floor.rooms) {
-    if (!room.feeds[mode]) continue;
-    const data = await fetchFeed(room.feeds[mode]);
+    if (!room.feeds[currentMetric]) continue;
+    const data = await fetchFeed(room.feeds[currentMetric]);
     if (data) onFeedData(room.entityId, data);
   }
 
   for (const space of floor.spaces) {
     for (const zone of space.zones) {
-      if (!zone.feeds[mode]) continue;
-      const data = await fetchFeed(zone.feeds[mode]);
+      if (!zone.feeds[currentMetric]) continue;
+      const data = await fetchFeed(zone.feeds[currentMetric]);
       if (data) onFeedData(zone.entityId, data);
     }
   }
@@ -207,11 +244,16 @@ async function onFeedData(entityId, data) {
   const zone = zones.get(entityId);
   const latest = data.timeseries?.[0]?.latest?.value;
 
+  const { colors, lookup } = gradients[currentMetric];
+
   if (elem && room) {
     console.log(elem.id, latest);
 
     if (latest) {
-      elem.setAttribute("fill", gradients[mode](latest));
+      elem.setAttribute(
+        "fill",
+        ColorInterpolator.toColorString(colors.interpolateColor(latest)),
+      );
       elem.setAttribute("title", latest);
     } else {
       elem.setAttribute("fill", noValueColour);
@@ -222,9 +264,9 @@ async function onFeedData(entityId, data) {
   if (zone) {
     console.debug(zone.zone.selector, latest);
 
-    if (latest) {
+    if (typeof latest === "number") {
       zone.point.value = latest;
-      zone.interpolator.update(zone.points, temperatureLookup);
+      zone.interpolator.update(zone.points, lookup);
       zone.draw();
     } else {
       zone.point.value = null;
